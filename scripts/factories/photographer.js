8cd7b64a4135108c203b9eb42f69689b849bb0b6
A_Larose <larose.alain@gmail.com>
2022-07-07 16:07:53 +0200

    mise en place des liens sur les cartes des photographes

--- scripts/factories/photographer.js
+++ scripts/factories/photographer.js
@@ -1,32 +1,42 @@
 function photographerFactory(data) {
-    const { name, portrait, city, country, tagline, price } = data;
+    const { id, name, portrait, city, country, tagline, price } = data;
+    const pageLink = `/photographer.html?id=${id}`;
     const picture = `assets/photographers/${portrait}`;
     const shortAddress = `${city}, ${country}`;
     const pricePerDay = `${price}€/jour`;
 
     function getUserCardDOM() {
+        //création de l'architecture de la card photographe
+        const linkPhotographer = document.createElement( 'a' );
+        linkPhotographer.setAttribute("href", pageLink);
+        linkPhotographer.setAttribute("class", 'linkPhotographer');
         const article = document.createElement( 'article' );
-        //création de l'image et du titre de la carte
+        const paragraph = document.createElement( 'p');
+
+        //création du contenu textuel de la card dans une balise p        
+        const address = document.createElement( 'address');
+        address.textContent = shortAddress;
+        const slogan = document.createElement( 'strong');
+        slogan.textContent = tagline;
+        //insertion des noeuds dans la balise p
+        paragraph.appendChild(address);
+        paragraph.appendChild(slogan);
+        slogan.insertAdjacentText('afterend', pricePerDay);
+
+        //création de l'en-tête de la card photographe:image et titre
         const img = document.createElement( 'img' );
         img.setAttribute("src", picture);
         img.setAttribute("alt", name);
         const h2 = document.createElement( 'h2' );
         h2.textContent = name;
-        //création des autres éléments dns une balise p
-        const paragraph = document.createElement( 'p');
-        const address = document.createElement( 'address');
-        address.textContent = shortAddress;
-        const slogan = document.createElement( 'strong');
-        slogan.textContent = tagline;
-        const newLine = document.createElement( 'BR');
-        //création des noeuds dans la balise p
-        paragraph.append(address, slogan, newLine, pricePerDay);
-        //création des noeuds dans la balise article
+        //création des noeuds constituant la card photographe dans la balise article
         article.appendChild(img);
         article.appendChild(h2);
         article.appendChild(paragraph);
-        
-        return (article);
+
+        //insertion de la card photographe dans une balise a pour créer le lien
+        linkPhotographer.appendChild(article);
+        return (linkPhotographer);
     }
     return { name, picture, getUserCardDOM }
 }
