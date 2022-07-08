435ae36aa019b82ebfe09d74708ede730885d2d8
A_Larose <larose.alain@gmail.com>
2022-07-08 18:07:06 +0200

    rapport d'accessibilité et corrections

--- scripts/factories/photographer.js
+++ scripts/factories/photographer.js
@@ -1,42 +1,40 @@
 function photographerFactory(data) {
-    const { id, name, portrait, city, country, tagline, price } = data;
-    const pageLink = `/photographer.html?id=${id}`;
+    const { name, portrait, city, country, tagline, price } = data;
     const picture = `assets/photographers/${portrait}`;
     const shortAddress = `${city}, ${country}`;
     const pricePerDay = `${price}€/jour`;
 
     function getUserCardDOM() {
         //création de l'architecture de la card photographe
-        const linkPhotographer = document.createElement( 'a' );
-        linkPhotographer.setAttribute("href", pageLink);
-        linkPhotographer.setAttribute("class", 'linkPhotographer');
         const article = document.createElement( 'article' );
-        const paragraph = document.createElement( 'p');
-
-        //création du contenu textuel de la card dans une balise p        
-        const address = document.createElement( 'address');
-        address.textContent = shortAddress;
-        const slogan = document.createElement( 'strong');
-        slogan.textContent = tagline;
-        //insertion des noeuds dans la balise p
-        paragraph.appendChild(address);
-        paragraph.appendChild(slogan);
-        slogan.insertAdjacentText('afterend', pricePerDay);
-
+        article.setAttribute("role", "article");
+        article.setAttribute("aria-label", "Cliquez pour voir les réalisations de");
+        article.setAttribute("tabindex", "0");
         //création de l'en-tête de la card photographe:image et titre
         const img = document.createElement( 'img' );
         img.setAttribute("src", picture);
-        img.setAttribute("alt", name);
+        img.setAttribute("role", img);
+        img.setAttribute("alt", "");
         const h2 = document.createElement( 'h2' );
         h2.textContent = name;
-        //création des noeuds constituant la card photographe dans la balise article
+        //création du contenu textuel de la card        
+        const address = document.createElement( 'address');
+        address.textContent = shortAddress;
+        const slogan = document.createElement( 'span');
+        slogan.textContent = tagline;
+        slogan.setAttribute("class", "sloganPhotographer");
+        const cost = document.createElement( 'span' );
+        cost.setAttribute("class", "pricePerDay");
+        cost.textContent = pricePerDay;
+        
+        //insertion des noeuds constituant la card photographe dans la balise article
         article.appendChild(img);
         article.appendChild(h2);
-        article.appendChild(paragraph);
-
-        //insertion de la card photographe dans une balise a pour créer le lien
-        linkPhotographer.appendChild(article);
-        return (linkPhotographer);
+        article.appendChild(address);
+        article.appendChild(slogan);
+        article.appendChild(cost);
+        //retourne la carte photographe
+        return (article);
     }
     return { name, picture, getUserCardDOM }
 }
--- scripts/pages/photographer.js
+++ scripts/pages/photographer.js
@@ -1,1 +1,8 @@
 //Mettre le code JavaScript lié à la page photographer.html
+
+/*
+const parameter = (new URL(document.location)).searchParams;
+const idPhotographer = params.get('id');
+
+const query = (photographers.find(ele => ele.id == 930))
+*/
